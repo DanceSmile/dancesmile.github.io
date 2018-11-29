e391f70230e611b6525af48bf01842d7f0539665
zero <zero@zero.com>
2018-11-29 11:59:48 +0800

    update docs

---
+++ deploy.js
@@ -0,0 +1,15 @@
+#!/usr/bin node
+var  exec  = require('child_process').exec
+
+var cmdStr = "git add .  && git commit -m 'update docs'  && git push "
+
+
+exec(cmdStr, function(err, stdout, stderr){
+
+    if(err){
+        console.log('has a lot of error: ' + stderr)
+    }else{
+        console.log('deploy done')
+    }
+
+})
