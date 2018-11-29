7eb36264e28d06fe142d1b35082031ced104e14d
zero <zero@zero.com>
2018-11-29 12:20:46 +0800

    update docs

--- deploy.js
+++ deploy.js
@@ -7,7 +7,7 @@ var cmdStr = "git add .  && git commit -m 'update docs'  && git push "
 exec(cmdStr, function(err, stdout, stderr){
 
     if(err){
-        console.log('has a lot of error: ' + stderr)
+        console.log('has a lot of error: ' + JSON.encode(stderr) )
     }else{
         console.log('deploy done')
     }
